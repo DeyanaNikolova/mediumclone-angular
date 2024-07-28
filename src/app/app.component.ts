98e5bcb790a364cbe979d30989bc954d816db823
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-07-28 14:13:49 +0100

    Remove components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
+  imports: [RouterOutlet],
   styleUrl: './app.component.scss',
 })
 export class AppComponent {}
