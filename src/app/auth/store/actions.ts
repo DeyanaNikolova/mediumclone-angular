62251df95c83414b9486f940dcee1ef91a6ba196
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-07-28 20:48:24 +0100

    Create actions group

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,7 +1,27 @@
-import { createAction, props } from '@ngrx/store';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
 import { RegisterRequestIntrface } from '../types/registerRequest.interface';
+import { CurrentUserInterface } from '../../shared/types/currentUser.interface';
 
-export const register = createAction(
-  '[Auth] Register',
-  props<{ request: RegisterRequestIntrface }>()
-);
+export const authActions = createActionGroup({
+  source: 'auth',
+  events: {
+    Register:  props<{ request: RegisterRequestIntrface }>(),
+    'Register success': props<{ currentUser: CurrentUserInterface}>(),
+    'Register failure': emptyProps(),
+  }
+})
+
+// export const register = createAction(
+//   '[Auth] Register',
+//   props<{ request: RegisterRequestIntrface }>()
+// );
+
+// export const registerSuccess = createAction(
+//   '[Auth] Register Success',
+//   props<{ request: RegisterRequestIntrface }>()
+// );
+
+// export const registerFailure = createAction(
+//   '[Auth] Register Failure',
+//   props<{ request: RegisterRequestIntrface }>()
+// );
