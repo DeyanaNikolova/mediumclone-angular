cf115a5ec5d352dc38c23416019758eab67e4ea3
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-07-28 20:49:46 +0100

    Update actions

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { register } from '../../store/actions';
+import { authActions } from '../../store/actions';
 import { RegisterRequestIntrface } from '../../types/registerRequest.interface';
 import { RouterLink } from '@angular/router';
 import { selectIsSubmitting } from '../../store/reducers';
@@ -31,6 +31,6 @@ export class RegisterComponent {
     const request: RegisterRequestIntrface = {
       user: this.form.getRawValue(),
     };
-    this.store.dispatch(register({ request }));
+    this.store.dispatch(authActions.register({ request }));
   }
 }
