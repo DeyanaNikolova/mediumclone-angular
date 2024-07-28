42cf8f6edcf49d241884288a388e061627e6c905
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-07-28 17:42:44 +0100

    Add provideStore to main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideStore } from '@ngrx/store';
 
-bootstrapApplication(AppComponent, { providers: [provideRouter(routes)] });
+bootstrapApplication(AppComponent, { providers: [provideRouter(routes), provideStore()] });
