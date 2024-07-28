fd239a3dbe8386982f9435185b37a1e80263a4fd
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-07-28 18:14:41 +0100

    Configure store-devtools

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,13 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { isDevMode } from '@angular/core';
 
-bootstrapApplication(AppComponent, { providers: [provideRouter(routes), provideStore()] });
+bootstrapApplication(AppComponent, { providers: [provideRouter(routes), provideStore(), provideStoreDevtools({
+    maxAge:25,
+    logOnly: !isDevMode(),
+    autoPause: true,
+    trace: false,
+    traceLimit:75,
+})] });
