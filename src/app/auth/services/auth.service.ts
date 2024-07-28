3bf21564eab2f54a9eb8de27d2cd7a4a316f1803
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-07-28 19:50:05 +0100

    Update url

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -4,6 +4,7 @@ import { RegisterRequestIntrface } from "../types/registerRequest.interface";
 import { Observable, map } from "rxjs";
 import { CurrentUserInterface } from "../../shared/types/currentUser.interface";
 import { AuthResponseInterface } from "../types/authResponse.interface";
+import { environment } from "../../../environments/environment.development";
 
 @Injectable({
     providedIn: 'root',
@@ -13,7 +14,7 @@ export class AuthService{
 constructor(private http: HttpClient) {}
 
 register(data: RegisterRequestIntrface): Observable<CurrentUserInterface>{
-    const url = 'https://api.realworld.io/api/users';
+    const url = environment.apiUrl + '/users'
 return this.http.post<AuthResponseInterface>(url, data)
 .pipe(map((response) => response.user))
 }
