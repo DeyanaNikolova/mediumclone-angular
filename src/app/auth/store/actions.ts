50b7e0818297c0bb0f0cffd1359bfdb57e10abf8
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-07-29 20:40:22 +0100

    Add props to register falure action

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,15 +1,16 @@
-import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { createActionGroup, props } from '@ngrx/store';
 import { RegisterRequestIntrface } from '../types/registerRequest.interface';
 import { CurrentUserInterface } from '../../shared/types/currentUser.interface';
+import { BackendErrorsInterface } from '../../shared/types/backendErrors.interface';
 
 export const authActions = createActionGroup({
   source: 'auth',
   events: {
-    Register:  props<{ request: RegisterRequestIntrface }>(),
-    'Register success': props<{ currentUser: CurrentUserInterface}>(),
-    'Register failure': emptyProps(),
-  }
-})
+    Register: props<{ request: RegisterRequestIntrface }>(),
+    'Register success': props<{ currentUser: CurrentUserInterface }>(),
+    'Register failure': props<{ errors: BackendErrorsInterface }>(),
+  },
+});
 
 // export const register = createAction(
 //   '[Auth] Register',
