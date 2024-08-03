59531ba88ae9af6fae1cfe511dd70cdbbf447f32
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-08-03 13:46:06 +0100

    Use persistance service in effects to set access token

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -5,14 +5,20 @@ import { authActions } from './actions';
 import { switchMap, map, catchError, of } from 'rxjs';
 import { CurrentUserInterface } from '../../shared/types/currentUser.interface';
 import { HttpErrorResponse } from '@angular/common/http';
+import { PersostanceService } from '../../shared/services/persistance.servise';
 
 export const registerEffect = createEffect(
-  (actions$ = inject(Actions), authService = inject(AuthService)) => {
+  (
+    actions$ = inject(Actions),
+    authService = inject(AuthService),
+    persistanceService = inject(PersostanceService)
+  ) => {
     return actions$.pipe(
       ofType(authActions.register),
       switchMap(({ request }) => {
         return authService.register(request).pipe(
           map((currentUser: CurrentUserInterface) => {
+            persistanceService.set('accessToken', currentUser.token);
             return authActions.registerSuccess({ currentUser });
           }),
           catchError((errorResponce: HttpErrorResponse) => {
