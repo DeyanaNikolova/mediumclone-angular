4fbd3d88cef98dd0df5863454f9066e1233ff6b4
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-08-03 15:15:21 +0100

    Add redirect to home page after successfull registration

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -2,16 +2,18 @@ import { createEffect, Actions, ofType } from '@ngrx/effects';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { authActions } from './actions';
-import { switchMap, map, catchError, of } from 'rxjs';
+import { switchMap, map, catchError, of, tap } from 'rxjs';
 import { CurrentUserInterface } from '../../shared/types/currentUser.interface';
 import { HttpErrorResponse } from '@angular/common/http';
 import { PersostanceService } from '../../shared/services/persistance.servise';
+import { Router } from '@angular/router';
 
 export const registerEffect = createEffect(
   (
     actions$ = inject(Actions),
     authService = inject(AuthService),
-    persistanceService = inject(PersostanceService)
+    persistanceService = inject(PersostanceService),
+    router = inject(Router),
   ) => {
     return actions$.pipe(
       ofType(authActions.register),
@@ -21,6 +23,7 @@ export const registerEffect = createEffect(
             persistanceService.set('accessToken', currentUser.token);
             return authActions.registerSuccess({ currentUser });
           }),
+          tap(()=>{ router.navigateByUrl('/') }),
           catchError((errorResponce: HttpErrorResponse) => {
             return of(
               authActions.registerFailure({
@@ -34,3 +37,16 @@ export const registerEffect = createEffect(
   },
   { functional: true }
 );
+
+// export const redirectAfterRegisterEffect = createEffect(
+//   (actions$ = inject(Actions), router = inject(Router)) => {
+//     return actions$.pipe(
+//       ofType(authActions.registerSuccess),
+//       tap(()=>{
+//         router.navigateByUrl('/')
+//       })
+//     )
+//   }, {
+//   functioanl: true,
+//   dispatch: false,
+// });
